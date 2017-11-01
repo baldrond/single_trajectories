b1481eeda7b5f76c9e01ce2d7c812fcb353d1215
balder <balderrh_94@hotmail.com>
2017-11-01 11:51:40 +0100

    commented:single_trajectories

--- single_trajectories.scala
+++ single_trajectories.scala
@@ -25,54 +25,46 @@ object single_trajectories {
 
     //Create distance matrix
     val dist_matrix_retur = cost_matrix.makeDistMatrix(cell_with_coords)
-    var dist_matrix = dist_matrix_retur._1
-    var dist_map = dist_matrix_retur._2
+    val dist_matrix = dist_matrix_retur._1
+    val dist_map = dist_matrix_retur._2
 
     //Create cost matrix
-    var coordinateMatrix_retur = cost_matrix.makeCoordinateMatrix(p, p2, dist_matrix, dist_map)
+    val coordinateMatrix_retur = cost_matrix.makeCoordinateMatrix(p, p2, dist_matrix, dist_map)
     var single_trajectories = coordinateMatrix_retur._3
     var matrix_entries = coordinateMatrix_retur._1
-    var columns_number = coordinateMatrix_retur._2
+    val columns_number = coordinateMatrix_retur._2
     var coordinate_matrix: CoordinateMatrix = new CoordinateMatrix(sc.parallelize(matrix_entries))
 
+    //Running through the hungarian algorithm
+    //step 1
     val step1_retur = hungarian_algorithm.step1(coordinate_matrix.toIndexedRowMatrix())
     if(step1_retur._1){
       coordinate_matrix = step1_retur._2.toCoordinateMatrix()
     }
-
+    //step 2: Not implemented yet as all columns will in the test example will contain at least one 1.0 value
     //TODO: Step 2
-    val step3_retur_init = hungarian_algorithm.step3(coordinate_matrix, matrix_entries, columns_number)
-    var rows = step3_retur_init._1
-    var columns = step3_retur_init._2
 
-    var sum1 = 0
-    for (row <- rows) {
-      sum1 += row
+    val number_of_iterations = 10
+    //Repeat steps 3–4 until an assignment is possible (In this test: only a few iterations)
+    for(i <- 0 until number_of_iterations) {
+      //step 3
+      val step3_retur = hungarian_algorithm.step3(coordinate_matrix, matrix_entries, columns_number)
+      val rows = step3_retur._1
+      var columns = step3_retur._2
+      val highest = hungarian_algorithm.step3_highest(coordinate_matrix.toIndexedRowMatrix(), rows)
+
+      val before = coordinate_matrix.entries.filter(entry => entry.i.toInt == highest._2._1 && entry.j.toInt == highest._2._2).first()
+      //step 4
+      matrix_entries = hungarian_algorithm.step4(coordinate_matrix, rows, highest._1)
+      coordinate_matrix = new CoordinateMatrix(sc.parallelize(matrix_entries))
+      val after = coordinate_matrix.entries.filter(entry => entry.i.toInt == highest._2._1 && entry.j.toInt == highest._2._2).first()
+
+      //Printing for test
+      print("Before: ")
+      println(before)
+      print("After: ")
+      println(after)
+
     }
-
-    val highest = hungarian_algorithm.step3_highest(coordinate_matrix.toIndexedRowMatrix(), rows)
-    println(highest)
-    println("Calculating new values")
-
-    val før = coordinate_matrix.entries.filter(entry => entry.i.toInt == highest._2._1 && entry.j.toInt == highest._2._2).first()
-    matrix_entries = hungarian_algorithm.step4(coordinate_matrix, rows, highest._1)
-    coordinate_matrix = new CoordinateMatrix(sc.parallelize(matrix_entries))
-    val etter = coordinate_matrix.entries.filter(entry => entry.i.toInt == highest._2._1 && entry.j.toInt == highest._2._2).first()
-    print("FØR: ")
-    println(før)
-    print("ETTER: ")
-    println(etter)
-
-    val step3_retur = hungarian_algorithm.step3(coordinate_matrix, matrix_entries, columns_number)
-    rows = step3_retur._1
-    columns = step3_retur._2
-
-    var sum2 = 0
-    for (row <- rows) {
-      sum2 += row
-    }
-
-    println(sum1)
-    println(sum2)
   }
 }
